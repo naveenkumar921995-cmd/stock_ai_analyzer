d2825b3da55117367d502def0bb732a7281f544b
naveenkumar921995-cmd <naveenkumar921995@gmail.com>
2026-02-17 22:22:17 +0530

    Update report_generator.py

--- modules/report_generator.py
+++ modules/report_generator.py
@@ -1,1 +1,17 @@
+from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer
+from reportlab.lib.styles import getSampleStyleSheet
+import streamlit as st
+
+def generate_pdf(stock):
+
+    doc = SimpleDocTemplate("stock_report.pdf")
+    styles = getSampleStyleSheet()
+    elements = []
+
+    elements.append(Paragraph(f"Stock Report - {stock}", styles['Title']))
+    elements.append(Spacer(1,12))
+
+    doc.build(elements)
+
+    st.success("PDF Generated Successfully")
 
