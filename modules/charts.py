d624fd920d2b741824da49e7b3c73e449054f8fa
naveenkumar921995-cmd <naveenkumar921995@gmail.com>
2026-02-17 22:21:47 +0530

    Update charts.py

--- modules/charts.py
+++ modules/charts.py
@@ -1,1 +1,18 @@
+import plotly.graph_objects as go
+import streamlit as st
+
+def show_price_chart(data):
+
+    fig = go.Figure()
+
+    fig.add_trace(go.Scatter(
+        x=data.index,
+        y=data['Close'],
+        mode='lines',
+        name='Close Price'
+    ))
+
+    fig.update_layout(template="plotly_dark", height=500)
+
+    st.plotly_chart(fig, use_container_width=True)
 
