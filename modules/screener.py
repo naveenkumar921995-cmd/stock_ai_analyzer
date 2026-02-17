ca903b481015ab34f7348e36ddee66c2143c7104
naveenkumar921995-cmd <naveenkumar921995@gmail.com>
2026-02-17 22:21:21 +0530

    Update screener.py

--- modules/screener.py
+++ modules/screener.py
@@ -1,1 +1,17 @@
+import streamlit as st
+
+def show_screener(info):
+
+    st.header("Screener Filter")
+
+    min_roe = st.slider("Minimum ROE %", 0, 30, 15)
+    max_pe = st.slider("Maximum PE", 5, 50, 25)
+
+    roe = info.get("returnOnEquity",0) * 100
+    pe = info.get("trailingPE",0)
+
+    if roe > min_roe and pe < max_pe:
+        st.success("Stock Passes Screener")
+    else:
+        st.warning("Stock Does Not Pass Screener")
 
