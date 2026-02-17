1a0e87c907729c92a46ad562f73b4f0f8302439b
naveenkumar921995-cmd <naveenkumar921995@gmail.com>
2026-02-17 22:19:14 +0530

    Update data_fetcher.py

--- modules/data_fetcher.py
+++ modules/data_fetcher.py
@@ -1,1 +1,10 @@
+import yfinance as yf
+
+def get_stock_data(symbol):
+    ticker = yf.Ticker(symbol)
+    info = ticker.info
+    history = ticker.history(period="2y")
+    financials = ticker.financials.T
+    cashflow = ticker.cashflow.T
+    return ticker, info, history, financials, cashflow
 
