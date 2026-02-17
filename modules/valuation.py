70f3d1b4ef8dd0d331b36fd3c4a33a3cf301ce4c
naveenkumar921995-cmd <naveenkumar921995@gmail.com>
2026-02-17 22:20:53 +0530

    Update valuation.py

--- modules/valuation.py
+++ modules/valuation.py
@@ -1,1 +1,24 @@
+import streamlit as st
+
+def show_dcf(cashflow, info):
+
+    st.header("Intrinsic Value (DCF Model)")
+
+    if cashflow.empty:
+        st.warning("No cash flow data available")
+        return
+
+    free_cash_flow = cashflow['Free Cash Flow'].iloc[0]
+
+    growth = st.slider("Growth Rate %", 1, 20, 10)
+    discount = st.slider("Discount Rate %", 5, 20, 12)
+
+    intrinsic = 0
+    for i in range(1,6):
+        intrinsic += (free_cash_flow * ((1+growth/100)**i)) / ((1+discount/100)**i)
+
+    shares = info.get("sharesOutstanding",1)
+    value = intrinsic / shares
+
+    st.success(f"Estimated Intrinsic Value: ₹{round(value,2)}")
 
